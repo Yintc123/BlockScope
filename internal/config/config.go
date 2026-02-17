9e748e4a7573c30243612976755742194a335d13
尹騰慶 <yintengching@YinMacBook-Pro.home>
2026-02-17 08:30:17 +0800

    Add the func, mustEnvInt, to convert the env value from string to integer.

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -21,11 +22,26 @@ type AppConfig struct {
 type DBConfig struct {
 	Driver   string
 	Host     string
-	Port     string
+	Port     int
 	User     string
 	Password string
 	Name     string
 	SSLMode  string
+}
+
+func mustEnvInt(key string) int {
+	var valueFromEnv string = os.Getenv(key)
+	if valueFromEnv == "" {
+		panic("Missing env: " + key)
+	}
+	var valueInt int
+	var err error
+	valueInt, err = strconv.Atoi(valueFromEnv)
+	if err != nil {
+		panic("invalid int env: " + key)
+	}
+
+	return valueInt
 }
 
 // LoadConfig 初始化 Config，根據 env 選擇 local / production
@@ -35,10 +51,18 @@ func LoadConfig(env string) *Config {
 		envToLower = "local"
 	}
 
-	godotenv.Load(".env." + envToLower)
+	// godotenv.Load(".env." + envToLower)
+	switch envToLower {
+	case "local":
+		godotenv.Load(".env.local")
+	case "production":
+		godotenv.Load(".env")
+	}
 
 	var commonChains []string = []string{"eth", "btc", "sol"}
 	var commonAppName string = "BlockScope"
+	// 由於 env 取得的值一定為 string，需要轉型為 int
+	var dbPort int = mustEnvInt(os.Getenv("DB_PORT"))
 
 	// 共用參數
 	var config *Config = &Config{
@@ -54,19 +78,19 @@ func LoadConfig(env string) *Config {
 	}
 
 	// 環境差異
-	switch strings.ToLower(env) {
+	switch envToLower {
 	case "local":
 		config.App.Port = "8080"
 		config.DB.Host = os.Getenv("DB_HOST")
-		config.DB.Port = os.Getenv("DB_PORT")
+		config.DB.Port = dbPort
 		config.DB.Name = os.Getenv("DB_NAME")
 		config.DB.User = os.Getenv("DB_USER")
 		config.DB.Password = os.Getenv("DB_PASSWORD")
 	case "production":
 		config.App.Port = "80"
 		config.DB.Host = os.Getenv("DB_HOST")
-		config.DB.Port = os.Getenv("DB_PORT")
-		config.DB.Name = os.Getenv("DB_Name")
+		config.DB.Port = dbPort
+		config.DB.Name = os.Getenv("DB_NAME")
 		config.DB.User = os.Getenv("DB_USER")
 		config.DB.Password = os.Getenv("DB_PASSWORD")
 	}
