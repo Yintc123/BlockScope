55684c8019d69e6fe35e4da0e85dee00c0282cbb
尹騰慶 <yintengching@Mac.home>
2026-02-20 12:24:35 +0800

    Update the domain, daily_active_address, on the type of the field, Date.

--- internal/domain/daily_active_address.go
+++ internal/domain/daily_active_address.go
@@ -3,8 +3,9 @@ package domain
 import "time"
 
 type DailyActiveAddress struct {
-	ID    uint      `gorm:"primaryKey"`
-	Date  time.Time `gorm:"index:idx_date_chain,unique"`
+	ID uint `gorm:"primaryKey"`
+	// 強制資料庫使用 date 類型，並在寫入時自動處理時區轉換
+	Date  time.Time `gorm:"type:date;index:idx_date_chain,unique"`
 	Chain string    `gorm:"index:idx_date_chain,unique"`
 	Count int64
 }
