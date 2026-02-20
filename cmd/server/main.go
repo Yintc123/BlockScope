9b15955cc50b01dda90ebc71e62210b19f5ff534
尹騰慶 <yintengching@Mac.home>
2026-02-20 12:05:27 +0800

    Update main.go for test architecture.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,44 +15,59 @@ import (
 )
 
 func main() {
-	// bootstrap 初始化所有依賴
-	router, port, err := bootstrap()
+	// 1. 取得環境變數，預設為 local
+	env := os.Getenv("APP_ENV")
+	if env == "" {
+		env = "local"
+	}
+
+	// 2. 啟動依賴注入與伺服器
+	router, port, err := bootstrap(env)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Critical boot error: %v", err)
 	}
 
 	// 啟動 Gin server
+	log.Printf("Server starting in [%s] mode on port %s", env, port)
 	if err := router.Run(":" + port); err != nil {
-		log.Fatal("fail to run server: ", err)
+		log.Fatalf("Server failed to run: %v", err)
 	}
 }
 
-// bootstrap 負責初始化 config / db / repository / service / handler
-func bootstrap() (*gin.Engine, string, error) {
-	// 1. 載入 config
-	cfg, err := config.LoadConfig(os.Getenv("APP_ENV"))
-
-	// 2. 初始化 DB
-	dbConn, err := db.NewDB(cfg.DB)
+// bootstrap 封裝所有初始化邏輯，負責初始化 config / db / repository / service / handler，提升可測試性
+func bootstrap(env string) (*gin.Engine, string, error) {
+	// A. 載入 config
+	cfg, err := config.LoadConfig(env)
 	if err != nil {
-		return nil, "", fmt.Errorf("fail to connect db: %w", err)
+		return nil, "", fmt.Errorf("config error: %w", err)
 	}
 
-	// 3. 初始化 repository / service / handler
+	// B. 初始化 DB
+	dbConn, err := db.NewDB(cfg.DB)
+	if err != nil {
+		return nil, "", fmt.Errorf("db error: %w", err)
+	}
+
+	// C. 依賴注入(Dependency Injection，DI)
+	// 每日活躍地址模組
 	repo := repository.NewDailyActiveAddressRepository(dbConn)
 	svc := service.NewDailyActiveAddressService(repo)
 	statsHandler := handler.NewStatsHandler(svc)
 
+	// 健康檢查模組
 	healthSvc := service.NewHealthCheckService(dbConn)
 	healthHandler := handler.NewHealthcheckHandler(healthSvc)
 
-	// 4. 初始化 router
+	// D. 初始化 router
 	router := gin.Default()
+
+	// 註冊根路由
 	http.RegisterRootRoutes(
 		router,
 		healthHandler,
 	)
 
+	// 註冊 stats 路由
 	statsGroup := router.Group("/stats")
 	http.RegisterStatsRoutes(
 		statsGroup,
