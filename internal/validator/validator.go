096a56ea58a92157c179b64fc18122e814c2b9e3
尹騰慶 <yiqazwsx123@gmail.com>
2026-03-01 18:52:03 +0800

    Add lowercase custom validation.

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,7 +1,39 @@
 package validator
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
+
 	"github.com/go-playground/validator/v10"
 )
 
-var Validator *validator.Validate = validator.New()
+var Validator *validator.Validate
+
+func init() {
+	fmt.Println("The package, validate, init starts...")
+	Validator = validator.New()
+
+	// 註冊自定義驗證 tag: "lowercase"
+	_ = Validator.RegisterValidation("lowercase", validateAndLowercase)
+}
+
+// 驗證欄位是否為字串並將其轉為小寫
+func validateAndLowercase(fieldLevel validator.FieldLevel) bool {
+	field := fieldLevel.Field()
+
+	// 確保欄位為字串型別，使用 reflect.String 來檢查欄位的型別
+	if field.Kind() != reflect.String {
+		return false
+	}
+
+	value := field.String()
+	// 將值轉為小寫
+	lowercased := strings.ToLower(value)
+
+	// 將小寫的值寫回結構體欄位中
+	field.SetString(lowercased)
+
+	// 驗證通過並且轉為小寫
+	return true
+}
