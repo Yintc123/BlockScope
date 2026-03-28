a3f08af746d8d5b2224f79d292bca5869a76f751
尹騰慶 <yiqazwsx123@gmail.com>
2026-03-28 16:06:35 +0800

    Update the way of starting API server on extracting the shared components between API server and crawler.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/Yintc123/BlockScope/internal/config"
-	"github.com/Yintc123/BlockScope/internal/db"
-	"github.com/Yintc123/BlockScope/internal/repository"
-	"github.com/Yintc123/BlockScope/internal/service"
+	"github.com/Yintc123/BlockScope/internal/app"
 	"github.com/Yintc123/BlockScope/internal/transport/http/handler"
 	"github.com/Yintc123/BlockScope/internal/transport/http/middleware"
 	http "github.com/Yintc123/BlockScope/internal/transport/http/routes"
@@ -16,67 +14,73 @@ import (
 )
 
 func main() {
+	// 定義啟動模式
+	mode := flag.String("mode", "api", "Mode: api, crawler or all")
+	flag.Parse()
+
+	switch *mode {
+	case "api":
+		runAPIServer() // 只啟動 API server
+	case "crawler":
+		runCrawler() // 只啟動 crawler
+	case "all":
+		// local 開發，同時啟動兩個實例
+		// 啟動有順序性，一定要先啟動 crawler，由於 API server 是阻塞(blocking)主執行緒
+		// 先啟動 crawler 在背景運作
+		go runCrawler()
+		// 再啟動 API server
+		runAPIServer()
+	default:
+		log.Fatal("Invalid mode")
+	}
+}
+
+func runAPIServer() {
 	// 1. 取得環境變數，預設為 local
-	env := os.Getenv("APP_ENV")
-	if env == "" {
-		env = "local"
-	}
+	env := getEnv()
 
-	// 2. 啟動依賴注入與伺服器
-	router, port, err := bootstrap(env)
+	// 1. 取得所有依賴
+	appDependencies, err := app.SetupDependencies(env)
 	if err != nil {
-		log.Fatalf("Critical boot error: %v", err)
+		log.Fatalf("Critical initial dependencies error: %v", err)
 	}
+	var appPort string = fmt.Sprintf("%d", appDependencies.Config.App.Port)
 
-	// 啟動 Gin server
-	log.Printf("Server starting in [%s] mode on port %s", env, port)
-	if err := router.Run(":" + port); err != nil {
+	// 2. 初始化 handlers
+	var handlers *handler.Handlers = handler.NewHandlers(appDependencies.Services)
+	// 3. 初始化 router
+	router, err := initRouter(handlers)
+	if err != nil {
+		log.Fatalf("Critical initial router error: %v", err)
+	}
+	// 4. 啟動 Gin server
+	log.Printf("Server starting in [%s] mode on port %s", env, appPort)
+	if err := router.Run(":" + appPort); err != nil {
 		log.Fatalf("Server failed to run: %v", err)
 	}
 }
 
-// bootstrap 封裝所有初始化邏輯，負責初始化 config / db / repository / service / handler，提升可測試性
-func bootstrap(env string) (*gin.Engine, string, error) {
-	// A. 載入 config
-	cfg, err := config.LoadConfig(env)
-	if err != nil {
-		return nil, "", fmt.Errorf("config error: %w", err)
-	}
-
-	// B. 初始化 DB
-	dbConn, err := db.NewDB(cfg.DB)
-	if err != nil {
-		return nil, "", fmt.Errorf("db error: %w", err)
-	}
-
-	// C. 依賴注入(Dependency Injection，DI)
-	// 每日活躍地址模組
-	repo := repository.NewDailyActiveAddressRepository(dbConn)
-	svc := service.NewDailyActiveAddressService(repo)
-	statsHandler := handler.NewStatsHandler(svc)
-
-	// 健康檢查模組
-	healthSvc := service.NewHealthCheckService(dbConn)
-	healthHandler := handler.NewHealthcheckHandler(healthSvc)
-
-	// D. 初始化 router
+func initRouter(handlers *handler.Handlers) (*gin.Engine, error) {
+	// 初始化 router
 	router := gin.Default()
-
 	// 註冊處理錯誤的 middleware
 	router.Use(middleware.ErrorHandler())
-
 	// 註冊根路由
-	http.RegisterRootRoutes(
-		router,
-		healthHandler,
-	)
-
+	http.RegisterRootRoutes(router, handlers.Healthcheck)
 	// 註冊 stats 路由
 	statsGroup := router.Group("/stats")
-	http.RegisterStatsRoutes(
-		statsGroup,
-		statsHandler,
-	)
+	http.RegisterStatsRoutes(statsGroup, handlers.Stats)
+	return router, nil
+}
 
-	return router, fmt.Sprintf("%d", cfg.App.Port), nil
+func runCrawler() {
+
+}
+
+func getEnv() string {
+	env := os.Getenv("APP_ENV")
+	if env == "" {
+		env = "local"
+	}
+	return env
 }
